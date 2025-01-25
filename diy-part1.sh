60845e9cbf96a7f9db48e54f10d57b5918290621
ssrugger <40933665+ssrugger@users.noreply.github.com>
2025-01-25 08:14:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,5 +19,5 @@
 #echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
 
 ### Kenzok8 packages ##
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
